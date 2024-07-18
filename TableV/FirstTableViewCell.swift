53c45a4f2f50022a23b238fbcc8ac29125f4dc27
nedaseyyedi <168231020+nedaseyyedi@users.noreply.github.com>
2024-07-18 23:38:50 +0300

    first commit

---
+++ TableV/FirstTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FirstTableViewCell.swift
+//  TableV
+//
+//  Created by EDA on 4/26/1403 AP.
+//
+
+import UIKit
+
+class FirstTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableV/Structure.swift
@@ -0,0 +1,8 @@
+//
+//  Structure.swift
+//  TableV
+//
+//  Created by EDA on 4/28/1403 AP.
+//
+
+import Foundation
